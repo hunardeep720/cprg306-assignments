0210ec0995e577a20cbdcb50a1c93c3907f91f87
hunardeep720 <123598156+hunardeep720@users.noreply.github.com>
2024-03-04 19:50:03 -0700

    week-6 changes

--- app/week-6/page.js
+++ app/week-6/page.js
@@ -1,18 +1,21 @@
 "use client";
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect} from "react";
 import NewItem from './new-item';
 import ItemList from './item-list';
 import itemsData from './items.json';
 export default function Page() {
     const [items, setItems] = useState(itemsData);
+    useEffect(() => {
+      let data = [];
+      data.concat(items);
+      <ItemList items={data} />;
+  }, [items]);
     const handleAddItem = (itemName,itemQuantity,itemCategory) => {
         const newItem = {name: itemName,quantity: itemQuantity,category: itemCategory};
         setItems([...items,newItem]);
-        console.log(setItems);
-        console.log(items);
+
     };
-    
     return (
       <main>
         <h1 className="text-4xl font-bold justify-center text-center">Shopping List</h1>
