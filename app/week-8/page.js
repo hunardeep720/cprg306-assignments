707dbbda7c4e96b075f8559ec5064a84afd364b7
hunardeep720 <123598156+hunardeep720@users.noreply.github.com>
2024-03-14 09:39:34 -0600

    week-8 optional

--- app/week-8/page.js
+++ app/week-8/page.js
@@ -1,14 +1,13 @@
 "use client";
-import Link from 'next/link';
-import { useState } from "react";
 import { useUserAuth } from "./_utils/auth-context";
 import Page from "./shopping-list/page.js";
 export default function page() {
-    const [log, setLog] = useState(false);
-  const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+  const { user, gitHubSignIn,googleSignIn} = useUserAuth();
   async function handleSignIn() {
     await gitHubSignIn();
-    setLog(true);
+  }
+  async function handleGoogleSignIn() {
+    await googleSignIn();
   }
   return (
     <main>
@@ -16,8 +15,9 @@ export default function page() {
         <Page/>
       ) : (
         <div className='flex flex-col items-center justify-center h-screen bg-white'>
-          <div><h1 className='font-bold text-black text-xl'>Click on sign on to sign with Github</h1></div>
-           <button className='m-5 bg-blue-500 w-32 h-14' onClick={handleSignIn}>Sign in</button> 
+          <div><h1 className='font-bold text-black text-xl'>Click on sign</h1></div>
+           <button className='m-5 bg-blue-500 w-32 h-14' onClick={handleSignIn}>Sign in with Github</button> 
+              <button className='m-5 bg-red-500 w-32 h-14' onClick={handleGoogleSignIn}>Sign in with Google</button>
         </div>
       )}
 
